dffa2c55b097a6cab76034fdc88e38df19df9f69
Greggory Heller <Gregg@Greggorys-MacBook-Pro-506.local>
2022-08-29 15:32:46 -0600

    updated default config.py

--- default_config.py
+++ default_config.py
@@ -1,1 +1,17 @@
-path = r"/Users/Gregg/Documents/MIT/SUR_lab/2021 Gregg rotation/kyle_data/ASC24"
+
+
+data_path = r"/Users/Gregg/Dropbox (MIT)/2021 Gregg rotation/kyle_data"
+
+collect_images_at_path = r"/Users/Gregg/Documents/MIT/SUR_lab/2021 Gregg rotation/annotated_images"
+#Images and plots will be placed here to easily perform a basic QC
+#and ensure that that distance matricies reflect the desired values
+
+
+precision = '%1.3f'
+#formatting string for numpy savetxt to determin how many decimals to include in the output csvs
+
+conversion_factor = .09 #pixels/micron for the image
+
+subfolder_name = 'dendritic_distance'
+#within each session directory, this subfolder will be created and contain
+#the detailed output of the sucessful algorithm (distance matricies, and other measurements)
